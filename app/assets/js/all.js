cf521d6f42e85c6c62e449be54902eae2f248b0b
Gill Chin <gill74123@gmail.com>
2021-11-29 18:02:01 +0800

    加入圖表、驗證表單欄位

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -27,6 +27,12 @@ axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelAp
 
     // 渲染初始頁面
     init(data);
+
+    // 渲染初始資料圓餅圖
+    areaRatio(data);
+  })
+  .catch(function (error) { // 呼叫 API 錯誤時可以印出錯誤
+    console.log(error);
   })
 
 // 渲染頁面
@@ -80,15 +86,17 @@ function checkInput() {
     if (input.value === "") {
       messageArea.innerHTML = `<span class="material-icons-outlined text-danger me-2">error</span>
       <span class="text-danger">必填！</span>`;
-      
     } else{
-      // 判斷星級是否在1~10分 & 判斷描述字數是否在100字以內
+      // 判斷星級是否在1~10分 & 判斷描述字數是否在100字內 & 判斷圖片網址是否為正確格式
       if (input.id === "ticketRate" && (parseInt(input.value) > 10 || parseInt(input.value) < 1)) {
         messageArea.innerHTML = `<span class="material-icons-outlined text-danger me-2">error</span>
       <span class="text-danger">星級區間是 1-10 分！</span>`;
       } else if (input.id === "ticketDescription" && input.value.length > 100) {
         messageArea.innerHTML = `<span class="material-icons-outlined text-danger me-2">error</span>
       <span class="text-danger">字數不得超過 100 個字！</span>`;
+      } else if (input.id === "ticketImgUrl" && validUrl(input)){
+        messageArea.innerHTML = `<span class="material-icons-outlined text-danger me-2">error</span>
+      <span class="text-danger">請填入正確圖片網址格式！</span>`;
       } else{
         messageArea.innerHTML = ``;
         num += 1;
@@ -97,6 +105,14 @@ function checkInput() {
   })
   // 回傳 num 結果給 addData() 判斷是否新增資料
   return num;
+}
+
+// 驗證圖片網址是否為正確格式
+function validUrl(input) {
+  let value = input.value;
+  if (!value.match(/jpg|jpeg|png|gif/i)) {
+    return true
+  }
 }
 
 // 新增套票
@@ -142,6 +158,9 @@ function updateList() {
 
   // 搜尋筆數 & 搜尋不到頁面渲染，用篩選好的 showData
   searchResultNum(showData);
+
+  // 渲染新的資料到圓餅圖
+  areaRatio(showData);
 }
 
 // 搜尋筆數 & 搜尋不頁面顯示
@@ -156,5 +175,60 @@ function searchResultNum(data) {
   } else{
     cantFindArea.classList.add("d-none");
   }
+}
+
+// C3 圖表
+function areaRatio(data) {
+  // output[['高雄', 1],['台北', 2],['台中', 3]]
+  // 將資料內所要用的 area 資料取出
+  let areaNum = {};
+  data.forEach(function(item){
+    if(areaNum[item.area]){
+      areaNum[item.area] += 1;
+    } else{
+      areaNum[item.area] = 1;
+    }
+  })
+  // console.log(areaNum);
+
+  // 將 areaNum 物件的 key 值取出並回傳陣列
+  let areaName = Object.keys(areaNum);
+  // console.log(areaName);
+
+  // 將 areaNum 物件的 value 取出並拼湊成 output 的樣子
+  let newArea = [];
+  areaName.forEach(function(item){
+    let ary = [];
+    ary.push(item);
+    ary.push(areaNum[item]);
+    newArea.push(ary);
+  })
+  // console.log(newArea);
+
+  let chart = c3.generate({
+    bindto: "#chart",
+    padding: {
+      bottom: 10
+    },
+    data: {
+      columns: newArea,
+      type: 'donut',
+      colors: {
+        '高雄': '#E68618',
+        '台中': '#5151D3',
+        '台北': '#26BFC7'
+      }
+    },
+    donut: {
+      title: "套票地區比重",
+      label: {
+        show: false
+      },
+      width: 15
+    },
+    size: {
+      height: 180
+    }
+  });
 }
 
